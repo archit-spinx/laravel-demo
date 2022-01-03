84d262ab7b39c1788c311a1779b4c443ceaa2de1
Vishal Padhariya <vishalp@development.local>
2022-01-03 11:00:15 +0530

    Set Layout | Header-Footer set

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+    </div>
+    <footer>
+        <p>&copy; Copyright by <b>SPINX Digital</b> {{ __(date('Y')) }} <br>
+        <a href="mailto:marco.spinx@gmail.com">Marco Wilson</a></p>
+    </footer>
+</body>
+</html>
