168c192a78f5b921f02c5355d40dbb4cb1d85ee6
Vishal Padhariya <vishalp@development.local>
2022-01-03 11:16:57 +0530

    change default page welcome to home

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
     </div>
     <footer>
-        <p>&copy; Copyright by <b>SPINX Digital</b> {{ __(date('Y')) }} <br>
+        <p>&copy; Copyright by <a href="https://www.spinxdigital.com" ><b>SPINX Digital</b></a> {{ __(date('Y')) }} <br>
         <a href="mailto:marco.spinx@gmail.com">Marco Wilson</a></p>
     </footer>
 </body>
