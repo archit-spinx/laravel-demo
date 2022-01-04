f191dcf2d1e0839e3fc3507a040a6f82c7e5051c
Vishal Padhariya <vishalp@development.local>
2022-01-04 17:36:26 +0530

    add-edit in one form

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/products', [App\Http\Controllers\ProductController::class, 'getProducts'])->name('products');
 Route::view('/add-product', 'add-product');
 Route::post('/add-product', [App\Http\Controllers\ProductController::class, 'create'])->name('add-product');
-Route::get('/edit-product/{id}', [App\Http\Controllers\ProductController::class, 'edit'])->name('edit-product');
-Route::post('/edit-product/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('edit-product');
+Route::get('/add-product/{id}', [App\Http\Controllers\ProductController::class, 'edit'])->name('edit-product');
+Route::post('/add-product/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('edit-product');
 Route::get('/products/remove/{id}', [App\Http\Controllers\ProductController::class, 'destroy']);
 Route::get('/search', [App\Http\Controllers\ProductController::class, 'search']);
 Route::get('/product/{id}', [App\Http\Controllers\ProductController::class, 'singleProduct']);
