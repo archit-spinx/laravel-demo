b5f645b8cccfa118106e18b67830d63eab7f0d42
mahesh-spinx <mahesh.spinx@gmail.com>
2022-01-10 12:11:55 +0530

    Update image field in update api

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -94,7 +94,9 @@ class ProductController extends BaseController
         $product->price = $input['price'];
         $product->special_price = $input['special_price'];
         $product->description = $input['description'];
-        $product->image = $input['image'];
+        if( isset($input['image']) ){
+            $product->image = $input['image'];
+        }
         $product->category_id = $input['category_id'];
         $product->update();
    
