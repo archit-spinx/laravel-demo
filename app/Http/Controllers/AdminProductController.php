5dd41011c8213fe40a25a937ce0c0937191cf902
mahesh-spinx <mahesh.spinx@gmail.com>
2022-02-10 17:18:39 +0530

    Add admin products pagination

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Client\Response;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AdminProductController extends Controller
 {
@@ -39,17 +41,29 @@ class AdminProductController extends Controller
     public function getProducts(Request $request)
     {
        
-        if(isset($_GET['page'])){
+        /* if(isset($_GET['page'])){
             $page = '?page='.$_GET['page'];
         }else{
             $page = '';
         }
-        $data = Http::get(env("API_URL").'/api/products'.$page);    
-        $products = $data->json();
-        return view('admin.adminproducts',compact(['products']));
+        $data = Http::get(env("API_URL").'/api/adminproducts'.$page);    
+        $products = $data->json();        
+        return view('admin.adminproducts',compact(['products'])); */
+
+        $categoryCollection = ProductCategory::all();
+        $data = Http::get(env("API_URL").'/api/products');
+        $product = $data->json();        
+        $productCollection = $this->arrayPaginator($product);
+        return view('admin.adminproducts',["productCollection" => $productCollection])->with("categoryCollection",$categoryCollection);
     }
 
-    
+    public function arrayPaginator($data){
+        $page = request()->get('page', 1);
+        $perPage =  request()->get('perPage', 6);
+        $offset = ($page * $perPage) - $perPage;
+        return new LengthAwarePaginator(array_slice($data, $offset, $perPage, true), count($data), $perPage, $page,
+        ['path' => Paginator::resolveCurrentPath()]);
+    }
 
     
 }
